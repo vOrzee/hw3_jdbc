4aa81338572ee952eafbbefe03eda001e53fa3d3
Roman Leshin <vorzee@gmail.com>
2024-05-10 06:41:33 +0500

    hw3.2

---
+++ src/main/resources/db/changelog/migrations/changelog-2.0.sql
@@ -0,0 +1,14 @@
+INSERT INTO customers (name, surname, age, phone_number) VALUES
+('Alexey', 'Ivanov', 30, '1234567890'),
+('Maria', 'Petrova', 25, '2345678901'),
+('Igor', 'Sidorov', 45, '3456789012'),
+('Olga', 'Kuznetsova', 35, '4567890123');
+
+INSERT INTO orders (date, customer_id, product_name, amount) VALUES
+('2024-01-01', 1, 'Laptop', 162344),
+('2024-01-02', 1, 'Smartphone', 22435),
+('2024-02-01', 2, 'Tablet', 134555),
+('2024-03-01', 3, 'Smartwatch', 12345),
+('2024-03-15', 4, 'E-reader', 18684),
+('2024-04-01', 2, 'Camera', 14325265),
+('2024-04-15', 4, 'Bluetooth Speaker', 22352);
