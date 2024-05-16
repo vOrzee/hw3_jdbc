3065ba3db93a16a159191029b750c9aa25e15404
Roman Leshin <vorzee@gmail.com>
2024-05-16 10:56:35 +0500

    hw 6 WebSecurityConfigurerAdapter (old version)

---
+++ src/main/java/ru/netology/hw3_jdbc/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package ru.netology.hw3_jdbc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/persons/by-city").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Ivan").password(passwordEncoder().encode("pass")).roles("TEEPOT");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
--- src/main/java/ru/netology/hw3_jdbc/entity/Person.java
+++ src/main/java/ru/netology/hw3_jdbc/entity/Person.java
@@ -1,6 +1,6 @@
 package ru.netology.hw3_jdbc.entity;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.*;
 
 @Getter
