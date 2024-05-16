1ef8f316b3c9118d5574310b8be291a100c4da9f
Roman Leshin <vorzee@gmail.com>
2024-05-16 10:44:01 +0500

    hw 6 EnableWebSecurity v1.0

---
+++ src/main/java/ru/netology/hw3_jdbc/controller/SecurityConfig.java
@@ -0,0 +1,47 @@
+package ru.netology.hw3_jdbc.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(withDefaults())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/persons/by-city").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("Ivan")
+                .password(passwordEncoder.encode("pass"))
+                .roles("TEEPOT")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
