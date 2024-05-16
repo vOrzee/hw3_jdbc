dd4b992426cf356df9372cd97c32caddcc398113
Roman Leshin <vorzee@gmail.com>
2024-05-16 06:42:56 +0500

    hw 5.2 v1.0

--- src/main/java/ru/netology/hw3_jdbc/repository/PostgresRepository.java
+++ src/main/java/ru/netology/hw3_jdbc/repository/PostgresRepository.java
@@ -2,6 +2,8 @@ package ru.netology.hw3_jdbc.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.netology.hw3_jdbc.entity.Person;
 
@@ -12,9 +14,12 @@ import java.util.Optional;
 @Repository
 public interface PostgresRepository extends JpaRepository<Person, Long> {
 
-    List<Person> findByCityOfLiving(String city);
+    @Query("select p from Person p where p.cityOfLiving = :city")
+    List<Person> findByCityOfLiving(@Param("city") String city);
 
-    List<Person> findByAgeLessThanOrderByAgeAsc(int age);
+    @Query("select p from Person p where p.age < :age order by p.age asc")
+    List<Person> findByAgeLessThanOrderByAgeAsc(@Param("age") int age);
 
-    Optional<Person> findByNameAndSurname(String name, String surname);
+    @Query("select p from Person p where lower(p.name) = lower(:name) and lower(p.surname) = lower(:surname)")
+    Optional<Person> findByNameAndSurname(@Param("name") String name, @Param("surname") String surname);
 }
